b548e6cc4bc25231779a4ccbc039aede95f1bd53
testgoofy <55271228+testgoofy@users.noreply.github.com>
2021-01-18 18:05:51 +0100

    feat: Dot product

--- src/main/java/ch/testgoofy/math/vector/Vector.java
+++ src/main/java/ch/testgoofy/math/vector/Vector.java
@@ -4,6 +4,32 @@ import ch.testgoofy.math.matrix.Matrix;
 
 public class Vector extends Matrix {
 
+  /**
+   * Calculates the dot product of tow vectors
+   * @param v1 First Vector to be multiplied
+   * @param v2 Second Vector to be multiplied
+   * @return  The dot product
+   */
+  public static double dot(Vector v1, Vector v2){
+    if (v1.rows != v2.rows || v1.columns != v2.columns){
+      throw new IllegalArgumentException("Vector 'v1' and Vector 'v2' don't have the same dimensions");
+    }
+    double product = 0;
+    for (int i = 0; i < v1.rows; i++) {
+      product += v1.data[i][0] * v2.data[i][0];
+    }
+    return product;
+  }
+
+  /**
+   * Calculates the dot product with the vector
+   * @param vector  The vector to be multiplied
+   * @return  The dot product
+   */
+  public double dot(Vector vector){
+    return dot((Vector) new Matrix(this.data), vector);
+  }
+
   public Vector(Vector vector){
     super((Matrix) vector);
   }
