7d19ae5083a6d88240838b20d57b8193cdd5d4ee
testgoofy <55271228+testgoofy@users.noreply.github.com>
2021-01-18 18:23:58 +0100

    chore: Update version tag to 0.1.0

--- src/main/java/ch/testgoofy/math/vector/Vector.java
+++ src/main/java/ch/testgoofy/math/vector/Vector.java
@@ -27,8 +27,8 @@ import ch.testgoofy.math.matrix.Matrix;
 /**
  * Represents a mathematical vector
  * @author  testgoofy
- * @since 1.0.0
- * @version 1.0.0
+ * @since 0.1.0
+ * @version 0.1.0
  */
 public class Vector extends Matrix {
 
